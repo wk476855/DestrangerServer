ac63364c39579948cc0ec9df1c6d617e59d2419b
ase_tangxm <ase_tangxm@123.com>
2015-05-09 16:52:49 +0800

    first

--- src/com/demo/destranger/tools/MessageManager.java
+++ src/com/demo/destranger/tools/MessageManager.java
@@ -1,5 +1,6 @@
 package com.demo.destranger.tools;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.json.JSONObject;
@@ -9,13 +10,13 @@ import com.tencent.xinge.XingeApp;
 
 public class MessageManager {
 
-	public boolean pushMessage(String username, String friendname, String message) {
+	/*public boolean pushMessage(String username, String friendname, String message) {
 		XingeApp push = new XingeApp(000, "A3CK257JL8AH");
 		Message msg = new Message();
 		msg.setTitle("message");
 		Map<String, Object> custom = new HashMap<String, Object>();
 		msg.setCustom(custom);
 		JSONObject json = push.pushSingleAccount(0, friendname, message);
-	}
+	}*/
 	
 }
